6256d93d1e0f64b155bc5d2eda6ff5fea7bd365f
drensokoli <79976300+drensokoli@users.noreply.github.com>
2022-10-04 18:09:51 +0200

    Completed Product API
    Implement methods: get, get by Id, post, put, delete, get by title, get by category.

---
+++ elefanti60/elefanti60/Data/AppDbContext .cs
@@ -0,0 +1,17 @@
+﻿using elefanti60.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace elefanti60.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
