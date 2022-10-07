d49b4591a03baa3fe0a3b840c6940d8a2ea37bed
ErmalMillaku09 <95182356+ErmalMillaku09@users.noreply.github.com>
2022-10-07 20:40:32 +0300

    Changed models and pulled cart model from cart branch

---
+++ elefanti60/elefanti60/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace elefanti60.Models
+{
+    public class Cart
+    {
+        public int NumberOfItems { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ elefanti60/elefanti60/Models/CartItem.cs
@@ -0,0 +1,21 @@
+﻿namespace elefanti60.Models
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; } = 1;
+        public decimal Price { get; set; }
+        public decimal Total
+        {
+            get { return Quantity * Price; }
+        }
+        public CartItem(Product product)
+        {
+            ProductId = product.Id;
+            ProductName = product.Title;
+            Price = product.Price;
+            Quantity = 1;
+        }
+    }
+}
