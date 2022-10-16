46c5b7dc25d0f1df976a4389b78d929d231f4160
Bledar Shala <bledaradam11@gmail.com>
2022-10-16 17:17:14 +0200

    Added more tests

--- elefanti60/TestProject1/UsersControllerTests.cs
+++ elefanti60/TestProject1/UsersControllerTests.cs
@@ -8,38 +8,89 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace TestProject1
 {
+    [TestClass]
      public class UsersControllerTests:TestBaseClass
     {
-        //[Fact] 
-        //public async Task Create_ShouldReturnOk_WhenUserCreated()
-        //{
-        //    var controller = new UsersController(_appDbContext);
 
-        //    var user = new User
-        //    {
-        //        Id = 0,
-        //        Name = "User",
-        //        LastName = "User",
-        //        Address = "Kosovo",
-        //        CardNumber = ""
+
+
+
+        [Fact]
+        public async Task Create_ShouldReturnOk_WhenUserCreated()
+        {
+            var controller = new UsersController(_appDbContext);
+
+            var user = new User
+            {
+
+                Id = 0,
+                Name = "User",
+                LastName = "User",
+                Address = "Kosovo",
+                CardNumber = 123,
+                Amount = 5,
+                DOB = new DateTime(2000,11,14),
+                Email = "Test@Gjirafa.com",
+                Password = "11112222",
+                Username = "Test Test"
                 
-                
-               
-        //    };
+            };
 
-        //    //Create method test
-        //    var result = (CreatedAtActionResult)controller.Create(user).Result;
-        //    result.StatusCode.Equals((int)HttpStatusCode.OK);
-        //    result.Value.Should().BeOfType<User>();
+            //Create method test
+            var result = (CreatedAtActionResult)controller.Create(user).Result;
+            result.StatusCode.Equals((int)HttpStatusCode.OK);
+            result.Value.Should().BeOfType<User>();
 
-        //    var userModel = (User)result.Value;
+            var userModel = (User)result.Value;
 
-        //    //Delete method test
-        //    var deleteResult = (NoContentResult)controller.Delete(userModel.Id).Result;
-        //    deleteResult.StatusCode.Equals((int)HttpStatusCode.NoContent);
-        //}
+            //Get method test
+
+            var get = controller.Get();
+            get.Should().NotBeNull();
+
+            //GetById method test 
+            var getById = (OkObjectResult)controller.GetByID(userModel.Id).Result;
+            result.StatusCode.Equals((int)HttpStatusCode.OK);
+
+            //GetByTitle method test
+            var getByTitle = controller.GetByTitle(userModel.Username).Result;
+            getByTitle.Equals(userModel.Username);
+
+            //Login method test
+            var login = (OkObjectResult)controller.Login(userModel.Username, userModel.Password).Result;
+            login.StatusCode.Equals((int)HttpStatusCode.OK);
+
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnNotFound_WhenUserDeleted()
+        {
+            var controller = new UsersController(_appDbContext);
+
+            var id = 999;
+
+            var result = (NotFoundResult)await controller.Delete(id);
+            result.StatusCode.Equals((int)HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnNotFound_WhenUserNotFound()
+        {
+            var controller = new UsersController(_appDbContext);
+
+            var id = 999;
+            
+            var result = (NotFoundResult)await controller.GetByID(id);
+            result.StatusCode.Equals((int)HttpStatusCode.NotFound);
+
+        }
+
+
+
     }
 }
